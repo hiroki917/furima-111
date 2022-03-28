c96684bb1a746766229cc5e106c93f344ac47386
hiroki917 <kensmith_5276_zemaitis@yahoo.co.jp>
2022-03-28 13:07:19 +0900

    price

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,23 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :edit, :update, :destroy, :create]
   def index
   end
 
   def new
     @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def item_params
+    params.require(:item).permit(:name, :explanation, :category_id, :status_id, :shipping_charge_id, :area_id, :day_id, :price,
+                                 :image).merge(user_id: current_user.id)
   end
 end
